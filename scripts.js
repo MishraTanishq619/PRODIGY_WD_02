28398ac15d6fdc7debcbf01a1c1bd8745c5f2e8f
Tanishq Mishra <mishratanishq619@gmail.com>
2023-11-21 14:51:04 +0000

    second

---
+++ scripts.js
@@ -0,0 +1,63 @@
+var min = document.getElementById("minute");
+var mins = 0;
+
+var seconds = document.getElementById("second");
+var sec = 0;
+
+var centiseconds = document.getElementById("centisecond");
+var centisecs = 0;
+
+var pause = 0;
+// functions
+
+function n(n) {
+	return n > 9 ? "" + n : "0" + n;
+}
+
+const counting = () => {
+	pause = 0;
+	setInterval(() => {
+		if (!pause) {
+			centisecs += 1;
+			centiseconds.innerHTML = n(centisecs);
+
+			if (centisecs >= 99) {
+				centisecs = 0;
+				sec += 1;
+
+				if (sec > 59) {
+					sec = 0;
+					mins += 1;
+					min.innerHTML = n(mins);
+				}
+				seconds.innerHTML = n(sec);
+			}
+		}
+	}, 10);
+};
+
+// const
+
+//
+
+var startbtn = document.getElementById("start");
+var pausebtn = document.getElementById("pause");
+var resetbtn = document.getElementById("reset");
+
+startbtn.addEventListener("click", () => {
+	counting();
+});
+
+pausebtn.addEventListener("click", () => {
+	pause = 1;
+});
+
+resetbtn.addEventListener("click", () => {
+	pause = 1;
+	mins = 0;
+	sec = 0;
+	centisecs = 0;
+	centiseconds.innerHTML = n(0);
+	min.innerHTML = n(0);
+	seconds.innerHTML = n(0);
+});
